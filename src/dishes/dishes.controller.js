ba354c36b992de2fc1809efd5710cddb81524e5a
Dylan Bruce <dyanb1127@gmail.com>
2022-03-01 21:14:25 -0500

    First commit

---
+++ src/dishes/dishes.controller.js
@@ -0,0 +1,12 @@
+//Add handlers and middleware functions to create, read, update, and list dishes.  Dishes CANNOT be deleted
+
+
+const path = require("path");
+
+// Use the existing dishes data
+const dishes = require(path.resolve("src/data/dishes-data"));
+
+// Use this function to assign ID's when necessary
+const nextId = require("../utils/nextId");
+
+// TODO: Implement the /dishes handlers needed to make the tests pass
