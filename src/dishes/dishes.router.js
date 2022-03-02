075dd0bd12c4321f55f65a837bce88c58f775c05
Dylan Bruce <dyanb1127@gmail.com>
2022-03-01 21:33:05 -0500

    General structure for routers

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,24 @@
+// Add two routes /dishes and /dishes/:dishId and attach the 
+// handlers [ create -post(controller.create), read -get(controller.read), 
+// update -put(controller.update), and list -get(controller.list) ] from dishes.controller
+
 const router = require("express").Router();
+const controller = require("./dishes.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+
+// dishes/:dishId - read, update
+router.route("/:dishId")
+  .get(controller.read)
+  .put(controller.update)
+  .all(methodNotAllowed);
+
+// /dishes - list, create
+router.route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .all(methodNotAllowed);
+
 
 module.exports = router;
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,26 @@
+// Add two routes /orders and /orders/:orderId and attach the 
+// handlers [ create -post(controller.create), read -get(controller.read), 
+// update -put(controller.update), delete -delete(controller.delete), and list -get(controller.list) ] 
+// from orders.controller
+
 const router = require("express").Router();
+const controller = require("./orders.controller");
+const methodNotAllowed = require("../errors/methodNotAllowed");
 
 // TODO: Implement the /orders routes needed to make the tests pass
+
+// orders/:orderId - read, update
+router.route("/:dishId")
+  .get(controller.read)
+  .put(controller.update)
+  .all(methodNotAllowed);
+
+// /orders - list, create, delete
+router.route("/")
+  .get(controller.list)
+  .post(controller.create)
+  .delete(controller.delete)
+  .all(methodNotAllowed);
+
 
 module.exports = router;
